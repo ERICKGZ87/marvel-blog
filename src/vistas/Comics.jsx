1ab21b009795fd72ba50ac2626a8cb910a7d465d
Erick GZ <erickgz87@gmail.com>
2021-11-23 20:44:20 -0300

    test

--- src/vistas/Comics.jsx
+++ src/vistas/Comics.jsx
@@ -1,86 +1,54 @@
-
-
 import { useParams, useHistory } from "react-router-dom";
-import { useContext, useEffect} from "react";
+import { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import CardComics from "../Componentes/CardComics";
-import Example from "../Componentes/VerTexto";
-
-
-
-
 
 const Comics = () => {
-    const { store,actions} = useContext(Context);
-    const Historia = useHistory()
-      
-    const {Heroe_id} = useParams();
+  const { store, actions } = useContext(Context);
+  const Historia = useHistory();
 
+  const { Heroe_id } = useParams();
 
-    //actions.getComics(Heroe_id);
+  //actions.getComics(Heroe_id);
 
+  useEffect(() => {
+   actions.getComics(Heroe_id);
+    console.log(Historia);
+  },);
 
-    useEffect(() => {
-      
-        actions.getComics(Heroe_id);
-        console.log(Historia)
-       }, [])
+  return (
+    <div className="Container">
+      <button className="btn btn-warning" onClick={Historia.goBack}>
+        Back
+      </button>
 
-    return (
-        <div className="Container">
-           <button className="btn btn-warning"
-                onClick={Historia.goBack}>
-                Back
+      <div className="row">
+        <h1>Lista de comics</h1>
 
-            </button>
+        {store.Comics !== null &&
+          Object.keys(store.Comics).map((par) => {
+            const value = store.Comics[par];
+            //let UrlWiki = value.urls[0].url
+            let exten = value.thumbnail.extension;
 
-            <div className="row">
-         
-            <h1>Lista de comics</h1>
-         
-                {
-                    store.Comics !== null &&
-                    Object.keys(store.Comics).map(par => {
-                        const value = store.Comics[par];
-                        //let UrlWiki = value.urls[0].url
-                        let exten = value.thumbnail.extension
-
-                        return (
-                            <>
-
-                                <div className="col-sm-4 py-10" key={value.id}>
-                            
-                       
-
-                            
-                                    <CardComics
-                               
-                                        NombreComics={value.title}
-                                        id={value.id}
-                                        descripcion={value.description}
-                                       
-                                
-                                        Url={value.thumbnail.path+"."+exten}
-                                        paginas={value.pageCount}
-                                        digitalID={value.digitalId}
-
-
-                                    />
-
-                                
-                                </div>
-                            </>
-                        )
-                    })
-                }
-
-
-
-            </div>
-
-        </div>
-    );
-
+            return (
+              <>
+                <div className="col-sm-4 py-10" key={value.id}>
+                  <CardComics
+                    NombreComics={value.title}
+                    id={value.id}
+                    descripcion={value.description}
+                    Url={value.thumbnail.path + "." + exten}
+                    paginas={value.pageCount}
+                    digitalID={value.digitalId}
+                  />
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Comics;
--- src/vistas/Home.jsx
+++ src/vistas/Home.jsx
@@ -1,31 +1,24 @@
 import Carrousel from "../Componentes/Carrousel";
 
- 
- const Home=()=>{
-
-
-    return(
-<>
-  
-     
-
- {    
-  <Carrousel className="carrousel"
-      item1="https://phantom-marca.unidadeditorial.es/838d5234c7803b705c60abc334975919/resize/1320/f/jpg/assets/multimedia/imagenes/2021/07/06/16255683165037.jpg"
-      item2="https://www.altonivel.com.mx/wp-content/uploads/2018/05/avengers.jpg"
-      item3="https://i.blogs.es/90e2ab/guardians0/1366_2000.jpeg"
-      item4="https://i.blogs.es/26abe2/1366_2000/450_1000.jpeg"
-      item5="https://img.ecartelera.com/noticias/fotos/60400/60446/1.jpg"
-      item6="https://lumiere-a.akamaihd.net/v1/images/keyart_es_ea2a4cef.jpeg?region=420,0,1080,1080&width=960"
-      item7="https://static.dw.com/image/52649694_303.jpg"
-      item8="https://thefreakhunterhome.files.wordpress.com/2021/01/disney-marvel-series.jpg"
-      item9="https://www.fmdos.cl/wp-content/uploads/2021/07/Estrenos-de-series-Marvel-2021-600x315.png"
-      item10="https://aws.glamour.mx/prod/designs/v1/assets/620x693/256601.jpeg"
-  />
- }
- </>
-    )
-
-
- }
- export default Home;
+const Home = () => {
+  return (
+    <>
+      {
+        <Carrousel
+          className="carrousel"
+          item1="https://phantom-marca.unidadeditorial.es/838d5234c7803b705c60abc334975919/resize/1320/f/jpg/assets/multimedia/imagenes/2021/07/06/16255683165037.jpg"
+          item2="https://www.altonivel.com.mx/wp-content/uploads/2018/05/avengers.jpg"
+          item3="https://i.blogs.es/90e2ab/guardians0/1366_2000.jpeg"
+          item4="https://i.blogs.es/26abe2/1366_2000/450_1000.jpeg"
+          item5="https://img.ecartelera.com/noticias/fotos/60400/60446/1.jpg"
+          item6="https://lumiere-a.akamaihd.net/v1/images/keyart_es_ea2a4cef.jpeg?region=420,0,1080,1080&width=960"
+          item7="https://static.dw.com/image/52649694_303.jpg"
+          item8="https://thefreakhunterhome.files.wordpress.com/2021/01/disney-marvel-series.jpg"
+          item9="https://www.fmdos.cl/wp-content/uploads/2021/07/Estrenos-de-series-Marvel-2021-600x315.png"
+          item10="https://aws.glamour.mx/prod/designs/v1/assets/620x693/256601.jpeg"
+        />
+      }
+    </>
+  );
+};
+export default Home;
